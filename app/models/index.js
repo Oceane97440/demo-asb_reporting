1b91986082da37e18b9cbd380c416fc2391da539
oceane97440 <sautronoceane98@gmail.com>
2020-10-07 10:17:35 +0400

    update

--- app/models/index.js
+++ app/models/index.js
@@ -1,15 +1,17 @@
-const dbConfig = require("../config/config.database");
+// const dbConfig = require("../config/config.database");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: "mysql",
+  operatorsAliases:false,
+
 
   pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 
