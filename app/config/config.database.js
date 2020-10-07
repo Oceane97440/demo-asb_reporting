e50d4cdd3f1c75fca1a4b483847c180fe27cb621
oceane97440 <sautronoceane98@gmail.com>
2020-10-07 10:04:46 +0400

    config

--- app/config/config.database.js
+++ app/config/config.database.js
@@ -1,9 +1,11 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "asb_reporting",
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
   dialect: "mysql",
+  operatorsAliases:false,
+
   pool: {
     max: 5,
     min: 0,
