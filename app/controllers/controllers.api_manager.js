f1fc393efc0b8b5c2a10029ccdbe6ba9f7e75b2e
oceane97440 <sautronoceane98@gmail.com>
2021-03-23 10:48:51 +0400

    add url mange

--- app/controllers/controllers.api_manager.js
+++ app/controllers/controllers.api_manager.js
@@ -57,7 +57,7 @@ exports.advertiser_add = async (req, res) => {
 
       var config = {
         method: 'GET',
-        url: 'https://manage.smartadserverapis.com/2044/advertisers/?offset=100&limit=100',
+        url: 'https://manage.smartadserverapis.com/2044/advertisers/?offset=200&limit=270',
         headers: {
           "Access-Control-Allow-Origin": "*",
           "Content-Type": "application/json"
@@ -145,7 +145,7 @@ exports.campaign_add = async (req, res) => {
 
       var config = {
         method: 'GET',
-        url: 'https://manage.smartadserverapis.com/2044/campaigns/?offset=100&limit=100',
+        url: 'https://manage.smartadserverapis.com/2044/campaigns/?offset=150&limit=155',
         headers: {
           "Access-Control-Allow-Origin": "*",
           "Content-Type": "application/json"
