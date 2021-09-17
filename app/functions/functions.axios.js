5691df6aa9012a7175b46cf39e9128017851205f
oceane97440 <sautronoceane98@gmail.com>
2021-09-17 09:24:26 +0400

    push function + publication de la branch

--- app/functions/functions.axios.js
+++ app/functions/functions.axios.js
@@ -301,3 +301,24 @@ exports.putManage = async (method, data = null) => {
 
   return test;
 }
+
+exports.getAdManager = async (campaign_id) => {
+
+  var test;
+
+  console.log('campaign_id' + campaign_id)
+
+
+  test = await axios({
+    method: 'GET',
+    url: 'http://localhost/api_google-manager/taskId/json/campaignID-'+campaign_id+'.json',
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-type": "Application/json"
+    }
+ 
+  })
+
+
+  return test;
+}
