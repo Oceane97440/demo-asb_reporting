ad8e8902e13b02298c3008db53576e5efc0e0c94
oceane97440 <sautronoceane98@gmail.com>
2021-10-12 17:33:48 +0400

    changement libélé

--- app/functions/functions.smartadserver.api.js
+++ app/functions/functions.smartadserver.api.js
@@ -89,7 +89,7 @@ exports.config = function (method, params = '') {
             username: dbApi.SMART_login,
             password: dbApi.SMART_password
         },
-       // params: {}
+        // params: {}
 
         /*
         //condition config
@@ -107,7 +107,7 @@ exports.config = function (method, params = '') {
             'limit': params.limit,
             'offset': params.offset,
         };
-   
+
 
         /*config['params'] = {
               limit:  params.limit,
@@ -171,85 +171,85 @@ exports.sortDataReport = function (formatSearch, dataObject) {
 
         // Récupére le nom des sites et les classes Créer les tableaux des sites
         if (site_name.match(/^\SM_LINFO.re{1}/igm)) {
-            sites.push(site_name);
+            sites.push('LINFO.RE (site)');
         }
         if (site_name.match(/^\SM_LINFO-ANDROID{1}/igm)) {
-            sites.push(site_name);
+            sites.push('LINFO.RE (app android)');
         }
         if (site_name.match(/^\SM_LINFO-IOS{1}/igm)) {
-            sites.push(site_name);
+            sites.push('LINFO.RE (app ios)');
         }
         if (site_name.match(/^\SM_ANTENNEREUNION{1}/igm)) {
-            sites.push(site_name);
+            sites.push('ANTENNEREUNION.FR (site)');
         }
         if (site_name.match(/^\SM_DOMTOMJOB{1}/igm)) {
-            sites.push(site_name);
+            sites.push('DOMTOMJOB.COM');
         }
         if (site_name.match(/^\SM_IMMO974{1}/igm)) {
-            sites.push(site_name);
+            sites.push('IMMO974.COM');
         }
         if (site_name.match(/^\SM_RODZAFER_LP{1}/igm)) {
-            sites.push(site_name);
+            sites.push('RODZAFER (site)');
         }
         if (site_name.match(/^\SM_RODZAFER_ANDROID{1}/igm)) {
-            sites.push(site_name);
+            sites.push('RODZAFER (app android');
         }
         if (site_name.match(/^\SM_RODZAFER_IOS{1}/igm)) {
-            sites.push(site_name);
+            sites.push('RODZAFER (app ios');
         }
         if (site_name.match(/^\SM_ORANGE_REUNION{1}/igm)) {
-            sites.push(site_name);
+            sites.push('ORANGE.RE (site)');
         }
         if (site_name.match(/^\SM_TF1{1}/igm)) {
-            sites.push(site_name);
+            sites.push('TF1 (site)');
         }
         if (site_name.match(/^\SM_M6{1}/igm)) {
-            sites.push(site_name);
+            sites.push('M6 (site)');
         }
         if (site_name.match(/^\SM_DAILYMOTION{1}/igm)) {
-            sites.push(site_name);
+            sites.push('DAILYMOTION (site)');
         }
         if (site_name.match(/^\SM_RODALI{1}/igm)) {
-            sites.push(site_name);
+            sites.push('RODALI (site)');
         }
 
         if (site_name.match(/^\N\/A{1}/i)) {
             // Si N/A
             if (insertion_name.match(/^\DOMTOMJOB{1}/igm)) {
-                sites.push('SM_DOMTOMJOB');
+                sites.push('DOMTOMJOB.COM');
             }
             if (insertion_name.match(/^\APPLI LINFO{1}/igm)) {
-                sites.push('SM_LINFO-ANDROID');
+                sites.push('LINFO.RE (app android)');
             }
             if (insertion_name.match(/^\LINFO{1}/igm)) {
-                sites.push('SM_LINFO.re');
+                sites.push('LINFO.RE (site)');
             }
             if (insertion_name.match(/^\ANTENNE REUNION{1}/igm)) {
-                sites.push('SM_ANTENNEREUNION');
+                sites.push('ANTENNEREUNION.FR (site)');
             }
             if (insertion_name.match(/^\ORANGE REUNION{1}/igm)) {
-                sites.push('SM_ANTENNEREUNION');
+                sites.push('ORANGE.RE (site)');
             }
             if (insertion_name.match(/^\RODZAFER{1}/igm)) {
-                sites.push('SM_ANTENNEREUNION');
+                sites.push('ANTENNEREUNION.FR (site)');
             }
             if (insertion_name.match(/^\RODALI{1}/igm)) {
-                sites.push('SM_ANTENNEREUNION');
+                sites.push('ANTENNEREUNION.FR (site)');
             }
             if (insertion_name.match(/^\IMMO974{1}/igm)) {
-                sites.push('SM_IMMO974');
+                sites.push('IMMO974.COM');
             }
             if (insertion_name.match(/^\TF1{1}/igm)) {
-                sites.push('SM_TF1');
+                sites.push('TF1 (site)');
             }
             if (insertion_name.match(/^\M6{1}/igm)) {
-                sites.push('SM_M6');
+                sites.push('M6 (site)');
             }
             if (insertion_name.match(/^\DAILYMOTION{1}/igm)) {
-                sites.push('SM_DAILYMOTION');
+                sites.push('DAILYMOTION (site)');
             }
 
-            sites.push('SM_LINFO.re');
+            sites.push('LINFO.RE (site)');
         }
     }
 
@@ -327,6 +327,7 @@ exports.sortDataReport = function (formatSearch, dataObject) {
             siteList = new Object();
             for (var ln = 0; ln < siteUniqueCount.length; ln++) {
                 sN = siteUniqueCount[ln];
+                console.log(sN)
                 siteImpressionsSUM = siteImpressions[sN].reduce(reducer);
                 siteClicksSUM = siteClicks[sN].reduce(reducer);
                 siteCompleteSUM = siteComplete[sN].reduce(reducer);
