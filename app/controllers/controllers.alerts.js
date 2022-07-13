4eb19c4b33daa386f3c5f58cfbf93e766d03bcd5
Oceane97440 <sautronoceane98@gmail.com>
2022-07-13 12:19:00 +0400

    modif mail

--- app/controllers/controllers.alerts.js
+++ app/controllers/controllers.alerts.js
@@ -990,11 +990,11 @@ exports.alert_manage_creative = async (req, res) => {
 
                 auth: {
                     user: "oceane.sautron@antennereunion.fr",
-                    pass: "s4utr0n_028"
+                    pass: process.env.EMAIL_PASS
                 },
                 from: "oceane.sautron@antennereunion.fr",
-                to: "oceane.sautron@antennereunion.fr",
-                //cc: "alvine.didier@antennereunion.fr",
+                to: "asb@antennereunion.fr",
+                cc: "alvine.didier@antennereunion.fr",
                 subject: 'Alerte Manage: Problème de programmation des créatives',
 
                 html: ' <head><style>font-family: Century Gothic;font-size: large; </style></head>Bonjour <br><br>  Tu trouveras ci-dessous le lien pour voir la liste des alertes du manage, problème de paramétrage des fichiers créatives(url/url clic) <b> </b> : <ul>' + listCreative.join('') + '</ul><ul>' + listCreativeUrlClic.join('') + '</ul><ul>'+listCreativeCompte.join('')+listCreativeCompteOther.join('')+listCreativeMobile.join('')+  listCreativeVideo.join('')+listCreativeMobile.join('')+listCreativeMobileHabillage.join('')+'</ul><br><br> À dispo pour échanger <br><br> <div style="font-size: 11pt;font-family: Calibri,sans-serif;"><img src="https://reporting.antennesb.fr/public/admin/photos/logo.png" width="79px" height="48px"><br><br><p><strong>L\'équipe Adtraffic</strong><br><small>Antenne Solutions Business<br><br> 2 rue Emile Hugot - Technopole de La Réunion<br> 97490 Sainte-Clotilde<br> Fixe : 0262 48 47 54<br> Fax : 0262 48 28 01 <br> Mobile : 0692 05 15 90<br> <a href="mailto:adtraffic@antennereunion.fr">adtraffic@antennereunion.fr</a></small></p></div>'
